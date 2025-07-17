efbd649292d052bd7335980df3e8a2a8cf210749
y <145238134+yynill@users.noreply.github.com>
2025-07-17 23:10:38 +0200

    simple cam - still of

--- src/main.c
+++ src/main.c
@@ -7,6 +7,7 @@ SDL_Window *window = NULL;
 SDL_Renderer *renderer = NULL;
 SDL_Point *origin = NULL;
 SDL_Texture *circle_texture = NULL;
+Camera *camera = NULL;
 
 int init_sdl() {
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
@@ -35,6 +36,15 @@ int init_sdl() {
 
     circle_texture = IMG_LoadTexture(renderer, "./assets/circle.png");
 
+    camera = malloc(sizeof(Camera));
+    camera->pos_x = 0;
+    camera->pos_y = 0;
+    camera->pos_z = 0;
+    camera->rotation_x = 0;
+    camera->rotation_y = 0;
+    camera->rotation_z = 0;
+    camera->zoom = 1;
+
     return 1;
 }
 
@@ -58,17 +68,66 @@ void cleanup_sdl(void) {
     SDL_Quit();
 }
 
+Point apply_camera(Point p) {
+    p.x += camera->pos_x;
+    p.y += camera->pos_y;
+    p.z += camera->pos_z;
+
+    // X-axis
+    float cos_x = cosf(camera->rotation_x);
+    float sin_x = sinf(camera->rotation_x);
+    float y1 = p.y * cos_x - p.z * sin_x;
+    float z1 = p.y * sin_x + p.z * cos_x;
+
+    // Y-axis
+    float cos_y = cosf(camera->rotation_y);
+    float sin_y = sinf(camera->rotation_y);
+    float x2 = p.x * cos_y + z1 * sin_y;
+    float z2 = -p.x * sin_y + z1 * cos_y;
+
+    // Z-axis
+    float cos_z = cosf(camera->rotation_z);
+    float sin_z = sinf(camera->rotation_z);
+    float x3 = x2 * cos_z - y1 * sin_z;
+    float y3 = x2 * sin_z + y1 * cos_z;
+
+    Point rotated = {(int)x3, (int)y3, (int)z2};
+    return rotated;
+}
+
 int main() {
     if (!init_sdl()) return 1;
 
     int running = 1;
     SDL_Event event;
-
+    const Uint8 *keyboard_state = SDL_GetKeyboardState(NULL);
 
     while (running) {
         while (SDL_PollEvent(&event)) {
             if (event.type == SDL_QUIT) running = 0;
         }
+
+        float rotation_speed = 0.05f;
+        float move_speed = 2.0f;
+
+        if (keyboard_state[SDL_SCANCODE_X]) {
+            if (keyboard_state[SDL_SCANCODE_UP]) camera->rotation_x += rotation_speed;
+            if (keyboard_state[SDL_SCANCODE_DOWN]) camera->rotation_x -= rotation_speed;
+        }
+        if (keyboard_state[SDL_SCANCODE_Y]) {
+            if (keyboard_state[SDL_SCANCODE_UP]) camera->rotation_y += rotation_speed;
+            if (keyboard_state[SDL_SCANCODE_DOWN]) camera->rotation_y -= rotation_speed;
+        }
+        if (keyboard_state[SDL_SCANCODE_Z]) {
+            if (keyboard_state[SDL_SCANCODE_UP]) camera->rotation_z += rotation_speed;
+            if (keyboard_state[SDL_SCANCODE_DOWN]) camera->rotation_z -= rotation_speed;
+        }
+        if (keyboard_state[SDL_SCANCODE_W]) camera->pos_z -= move_speed;
+        if (keyboard_state[SDL_SCANCODE_S]) camera->pos_z += move_speed;
+        if (keyboard_state[SDL_SCANCODE_A]) camera->pos_x -= move_speed;
+        if (keyboard_state[SDL_SCANCODE_D]) camera->pos_x += move_speed;
+        if (keyboard_state[SDL_SCANCODE_Q]) camera->pos_y -= move_speed;
+        if (keyboard_state[SDL_SCANCODE_E]) camera->pos_y += move_speed;
 
         SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
         SDL_RenderClear(renderer);
@@ -98,6 +157,38 @@ int main() {
         draw_line(p5, p7, white);
         draw_line(p6, p7, white);
 
+        // draw_square_face();
+
+        // Point q0 = {200 - 50, 200 - 50, 200 - 50};
+        // Point q1 = {200 - 50, 200 - 50, 200 + 50};
+        // Point q2 = {200 - 50, 200 + 50, 200 - 50};
+        // Point q3 = {200 - 50, 200 + 50, 200 + 50};
+        // Point q4 = {200 + 50, 200 - 50, 200 - 50};
+        // Point q5 = {200 + 50, 200 - 50, 200 + 50};
+        // Point q6 = {200 + 50, 200 + 50, 200 - 50};
+        // Point q7 = {200 + 50, 200 + 50, 200 + 50};
+
+        // SDL_Color green = {0, 255, 0, 255};
+
+        // draw_line(q0, q1, green);
+        // draw_line(q0, q2, green);
+        // draw_line(q0, q4, green);
+        // draw_line(q1, q3, green);
+        // draw_line(q1, q5, green);
+        // draw_line(q2, q3, green);
+        // draw_line(q2, q6, green);
+        // draw_line(q3, q7, green);
+        // draw_line(q4, q5, green);
+        // draw_line(q4, q6, green);
+        // draw_line(q5, q7, green);
+        // draw_line(q6, q7, green);
+
+        // Sint16 a = (Sint16)1000000000000000;
+        // Sint16 b = (Sint16)1000000000000000;
+
+        // SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+        // filledPolygonRGBA(renderer, &a, &b, 1000, 255, 255, 255, 255);
+
         SDL_RenderPresent(renderer);
         SDL_Delay(16);
     }
@@ -122,8 +213,10 @@ void draw_origin() {
 }
 
 void draw_point(Point p, SDL_Color color) {
-    float x_2d = origin->x + p.y + (-0.5f * p.x);
-    float y_2d = origin->y - p.z + (0.5f * p.x);
+    Point rotated = apply_camera(p);
+
+    float x_2d = origin->x + rotated.y + (-0.5f * rotated.x);
+    float y_2d = origin->y - rotated.z + (0.5f * rotated.x);
 
     int radius = 2;
     SDL_Rect circle_rect = {(int)x_2d - radius, (int)y_2d - radius, radius * 2, radius * 2};
@@ -133,11 +226,18 @@ void draw_point(Point p, SDL_Color color) {
 }
 
 void draw_line(Point p1, Point p2, SDL_Color color) {
-    float x1_2d = origin->x + p1.y + (-0.5f * p1.x);
-    float y1_2d = origin->y - p1.z + (0.5f * p1.x);
-    float x2_2d = origin->x + p2.y + (-0.5f * p2.x);
-    float y2_2d = origin->y - p2.z + (0.5f * p2.x);
+    Point rotated_p1 = apply_camera(p1);
+    Point rotated_p2 = apply_camera(p2);
+
+    float x1_2d = origin->x + rotated_p1.y + (-0.5f * rotated_p1.x);
+    float y1_2d = origin->y - rotated_p1.z + (0.5f * rotated_p1.x);
+    float x2_2d = origin->x + rotated_p2.y + (-0.5f * rotated_p2.x);
+    float y2_2d = origin->y - rotated_p2.z + (0.5f * rotated_p2.x);
 
     SDL_SetRenderDrawColor(renderer, color.r, color.g, color.b, color.a);
     SDL_RenderDrawLine(renderer, (int)x1_2d, (int)y1_2d, (int)x2_2d, (int)y2_2d);
 }
+
+// void draw_square_face(Point p1, Point p2, Point p3, Point p4, SDL_Color color) {
+
+// }
--- src/main.h
+++ src/main.h
@@ -8,6 +8,7 @@
 
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
+#include <SDL2/SDL2_gfxPrimitives.h>
 
 typedef struct Point {
     int x;
@@ -15,16 +16,30 @@ typedef struct Point {
     int z;
 } Point;
 
+typedef struct Camera {
+    float pos_x;
+    float pos_y;
+    float pos_z;
+
+    float rotation_x;
+    float rotation_y;
+    float rotation_z;
+
+    float zoom;
+} Camera;
+
 extern SDL_Window *window;
 extern SDL_Renderer *renderer;
 extern SDL_Point *origin;
 extern SDL_Texture *circle_texture;
-
+extern Camera *camera;
 
 int init_sdl(void);
 void cleanup_sdl(void);
 void draw_origin();
 void draw_point(Point p, SDL_Color color);
 void draw_line(Point p1, Point p2, SDL_Color color);
+// void draw_square_face(Point p1, Point p2, Point p3, Point p4, SDL_Color color);
+Point apply_camera(Point p);
 
 #endif // MAIN_H
